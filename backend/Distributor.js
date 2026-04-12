aff9c57e6a03b7519ac373a9661289009e5a7a75
Hasib Rasul <hasibrasul@Hasibs-MacBook-Air.local>
2026-04-12 18:04:35 +0300

    Week 6: Implemented Smart Circular Distribution and Node Redundancy(Updated in Week 7)

--- backend/Distributor.js
+++ backend/Distributor.js
@@ -1,5 +1,11 @@
 const fs = require('fs');
 
+// Import the data from Merkle script
+const { targetFile, finalRoot } = require('./Merkle_Generator');
+
+// Get the pure name without the path
+const pureFileName = targetFile.split('/').pop();
+
 // These are the Storage Nodes
 const nodes = [
     './Storage_Server/Node1/',
@@ -15,10 +21,11 @@ const allFiles = fs.readdirSync('./');
 // We need a counter to keep track of which shard we are in
 let shardCount = 0;
 
+let shardTracking = []; 
+
 allFiles.forEach((fileName) => {
-    
-    // Check: Is this a .bin shard?
-    if (fileName.endsWith('.bin')) {
+    // Only process shards that belong to the CURRENT file
+    if (fileName.endsWith('.bin') && fileName.startsWith(pureFileName)) {
         console.log("Processing shard: " + fileName);
 
         // The Circular Math
@@ -33,9 +40,21 @@ allFiles.forEach((fileName) => {
         fs.copyFileSync('./' + fileName, backupNode + fileName);
         console.log(" Saved " + fileName + " to " + backupNode);
 
+        // WEEK 7 ADDITION: Save the location info for the JSON Map
+        shardTracking.push({
+            id: fileName,
+            locations: [`Node${(shardCount % 3) + 1}`, `Node${((shardCount + 1) % 3) + 1}`]
+        });
+
         // Move to the next shard number
         shardCount++;
+
+        // CLEANUP: Delete the shard from the main folder after moving it
+        fs.unlinkSync('./' + fileName);
     }
 });
 
 console.log("-- Distribution Completed: Efficient and Redundant --");
+
+// Export the variables for the Map Generator to use
+module.exports = { targetFile: pureFileName, finalRoot, shardTracking }; 
